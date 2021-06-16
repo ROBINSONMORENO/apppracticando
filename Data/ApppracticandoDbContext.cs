d65b3157d30467aac529def7fbf0c277aa68896d
ROBINSON MORENO <robinson.moreno@upch.pe>
2021-06-15 19:21:56 -0500

    migracion membresia

--- Data/ApppracticandoDbContext.cs
+++ Data/ApppracticandoDbContext.cs
@@ -4,11 +4,11 @@ using System.Text;
 using Microsoft.EntityFrameworkCore;
 using apppracticando.Models;
 using System.ComponentModel.DataAnnotations;
-
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace apppracticando.Data
 {
-    public class ApppracticandoDbContext : DbContext
+    public class ApppracticandoDbContext : IdentityDbContext
     {       
         public DbSet<Fail> Fails { get; set; }
         public DbSet<Comentario> Comentarios { get; set; }
